cac243b111770b222a9968e79cc4ad7024b957bd
JoaquinPan <boson159456@gmail.com>
2024-02-25 20:40:21 +0800

    Compelete persistence

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+let listState = [];
+
+const STATE_KEY = "todo-list";
+
+function loadState() {
+  const listState = localStorage.getItem(STATE_KEY);
+  if (listState !== null) {
+    return JSON.parse(listState);
+  }
+  return [];
+}
+
+function saveState(list) {
+  localStorage.setItem(STATE_KEY, JSON.stringify(list));
+}
+
+function initList() {
+  // load state
+  listState = loadState();
+  //render list
+  const ul = document.getElementById("list");
+  for (const item of listState) {
+    const li = document.getElementById("li");
+    li.innerText = item.text;
+
+    const deleteButton = document.createElement("span");
+    deleteButton.classList.add("delete");
+    deleteButton.onclick = deleteItem;
+    li.appendChild(deleteButton);
+
+    li.classList.add("item");
+    if (item.checked) {
+      li.classList.add("checked");
+    }
+    li.onclick = checkItem;
+
+    ul.appendChild(li);
+  }
+}
+
 function addItem() {
   const ul = document.getElementById("list");
   const input = document.getElementById("input");
@@ -19,20 +59,38 @@ function addItem() {
 
   newItem.appendChild(deleteButton);
 
+  listState.push({
+    text,
+    checked: false,
+  });
+  saveState(listState);
+
   input.value = "";
   ul.appendChild(newItem);
 }
 
-function checkItem() {
-  const item = this;
+function checkItem(e) {
+  const item = e.target;
+  const parent = item.parentNode;
+  const idx = Array.from(parent.childNodes).indexOf(item);
+
+  listState[idx].checked = !listState[idx].checked;
+
   item.classList.toggle("checked");
+  saveState(listState);
 }
 
-function deleteItem() {
+function deleteItem(e) {
   const item = this.parentNode;
   const parent = item.parentNode;
+  const idx = Array.from(parent.childNodes).indexOf(item);
+  listState = listState.filter((_, i) => i !== idx);
   parent.removeChild(item);
+  saveState(listState);
+  e.stopPeopagation();
 }
+
+initList();
 
 const addButton = document.getElementById("add-button");
 addButton.addEventListener("click", addItem);
